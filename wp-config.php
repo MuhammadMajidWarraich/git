68bda8251f87ebb9fc40c3086be55f5bedd4557f
Muhammad Majid <mmajid@ad.mcmso.co>
2022-03-11 12:02:37 +0500

    wp-config updated

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'majid' );
+define( 'DB_NAME', 'git' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
